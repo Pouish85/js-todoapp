959120f2a7ac3cb8b25f30528c029954902ebb49
Pouish85 <pouish.iphone@gmail.com>
2023-01-13 09:55:54 +0100

    feat: affichage de la liste des taches

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const ulContainer = document.querySelector('ul');
+
+const todos = [
+    {text: "Aller faire les courses", done: false},
+    {text: "Sortir le chien", done: true},
+]
+
+//fonction fléchée qui va creer et remplir une balise <li>
+const createTodoElement = (todo) => {
+    const li = document.createElement('li');
+    li.classList.add("d-flex", "align-items-start")
+    li.innerHTML = `
+        <span class="todo ${todo.done ? 'done' : ''}"></span>
+        <p class="w-100">${todo.text}</p>
+        <button class="btn btn-primary mx-2">Editer</button>
+        <button class="btn btn-danger mx-2">Supprimer</button>
+    `
+
+    return li;
+}
+
+// fonction qui va afficher la liste des taches
+const displayTodos = () => {
+    const todosNode = todos.map((todo, index) => {
+        return createTodoElement(todo);
+    })
+
+    ulContainer.innerHTML = '';
+    ulContainer.append(...todosNode)
+
+}
+
+displayTodos();
